f255be0c19a42d00c7beabac3179cf9a8b22845e
RickDnamps <103229273+RickDnamps@users.noreply.github.com>
2026-04-21 00:49:41 -0400

    feat: settings page with dark/light theme toggle

--- src/pages/SettingsPage.tsx
+++ src/pages/SettingsPage.tsx
@@ -1,15 +1,34 @@
-import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton, IonButtons } from '@ionic/react';
+import {
+  IonPage, IonHeader, IonToolbar, IonTitle, IonContent, IonMenuButton,
+  IonButtons, IonList, IonItem, IonLabel, IonToggle,
+} from '@ionic/react';
+import { useMotorStore } from '../store/motorStore';
 
-const SettingsPage: React.FC = () => (
-  <IonPage>
-    <IonHeader>
-      <IonToolbar>
-        <IonButtons slot="start"><IonMenuButton /></IonButtons>
-        <IonTitle>App Settings</IonTitle>
-      </IonToolbar>
-    </IonHeader>
-    <IonContent />
-  </IonPage>
-);
+const SettingsPage: React.FC = () => {
+  const { darkMode, setDarkMode } = useMotorStore();
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonButtons slot="start"><IonMenuButton /></IonButtons>
+          <IonTitle>App Settings</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent>
+        <IonList>
+          <IonItem>
+            <IonLabel>Dark Mode</IonLabel>
+            <IonToggle
+              slot="end"
+              checked={darkMode}
+              onIonChange={(e) => setDarkMode(e.detail.checked)}
+            />
+          </IonItem>
+        </IonList>
+      </IonContent>
+    </IonPage>
+  );
+};
 
 export default SettingsPage;
